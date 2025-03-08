28bcf88640685d2d744cfdd0e33144eddce71b18
3roland59 <amarteyroland359@gmail.com>
2025-03-08 09:32:12 +0000

    edited staticroot

--- CampuShelf/settings.py
+++ CampuShelf/settings.py
@@ -163,7 +163,8 @@ STATIC_URL = "static/"
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = os.path.join(MEDIA_BASE_DIR, "media")
 
